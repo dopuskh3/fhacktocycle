ee195dd6d4c7c4ba57725e7a08fab06a414cfba3
Francois Visconte <f.visconte@gmail.com>
2013-09-29 11:47:59 +0200

    Limit the number of returned velov stations.

--- backend/fhacktocycle/fhacktocycle/apis/velov.py
+++ backend/fhacktocycle/fhacktocycle/apis/velov.py
@@ -2,6 +2,8 @@ import json
 import urllib
 import time
 from fhacktocycle.apis import Api, Item
+from django.contrib.gis.geos import Point, GEOSGeometry
+from django.contrib.gis.measure import D
 
 
 class VelovApi(Api):
@@ -15,9 +17,12 @@ class VelovApi(Api):
     url = self.VELOV_API_URL_PATTERN
     velov = json.loads(urllib.urlopen(url).read())
     for station in velov['values']:
-      yield Item(station['name'],
-                 'velov',
-                 '%s: %s disponibles, %s places' % (  station['address'], station['available_bikes'], station['available_bike_stands']),
-                 station['lat'],
-                 station['lng'])
+      station_pos = GEOSGeometry('POINT(%s %s)' % ( station['lng'], station['lat']))
+      current_pos = GEOSGeometry('POINT(%f %f)' %  (lng, lat))
+      if station_pos.distance(current_pos) * 100 < 0.8:
+        yield Item(station['name'],
+                  'velov',
+                  '%s: %s disponibles, %s places' % (  station['address'], station['available_bikes'], station['available_bike_stands']),
+                  station['lat'],
+                  station['lng'])
 
