7dc2693342450057e4c5cb1ee4b06ede9f5a8432
Pierre-Baptiste BECHU <pbechu@users.noreply.github.com>
2013-09-29 10:56:28 +0200

    continous request

--- frontend/js/base.js
+++ frontend/js/base.js
@@ -25,11 +25,11 @@ this.module('base', function() {
   }
   //this.update();
   //setInterval(update, 10000);
-  helpers.updateLocation(samples.myLocation);
-  helpers.makeRequest(samples.myLocation);
+  
   setInterval(function() {  samples.myLocation.latitude -= 0.0005;
                             samples.myLocation.longitude += 0.0004;
                             helpers.updateLocation(samples.myLocation);
+                            helpers.makeRequest(samples.myLocation);
                              }, 5000);
   //mock update
 
