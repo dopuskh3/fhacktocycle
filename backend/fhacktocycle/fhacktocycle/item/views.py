d09bf3723697bcbbdd022208692ab740f39040c0
Francois Visconte <f.visconte@gmail.com>
2013-09-29 10:15:25 +0200

    Display shelters condition

--- backend/fhacktocycle/fhacktocycle/item/views.py
+++ backend/fhacktocycle/fhacktocycle/item/views.py
@@ -5,13 +5,24 @@ from django.views.generic import View
 from django.contrib.gis.geos import Point, GEOSGeometry
 from django.contrib.gis.measure import D
 from fhacktocycle.item.models import Items
+from fhacktocycle.apis.weather import WeatherApi
 
 
 def searchItems(lat, lng):
   items = []
+  display_shelters = False
+  weather = [ w for w in WeatherApi().get(lat, lng) ][0]
+  weather_string = weather.title.lower()
+
+  if 'pluie' in weather_string or 'nuage' in weather_string:
+    display_shelters = True
+
   for api in settings.ENABLED_APIS:
     for item in api().get(lat, lng):
-      items.append(item.toDict())
+      if (item.type == 'shelter' and display_shelters == True) or \
+         item.type != 'shelter':
+        items.append(item.toDict())
+
   return items
 
 
